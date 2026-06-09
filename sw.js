c3d9f0bb9253f3ef7aa3993edc0f1a4a0d3f5e85
ludolu26 <lupsg77@gmail.com>
2026-06-09 19:21:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+var CACHE_NAME = 'homsecur-v1';
+var urlsToCache = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
